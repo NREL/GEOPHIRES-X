ca9a90f4a47be1b65b92d3a1c0e7428ab853ea74
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2023-10-04 14:17:13 -0700

    Re-enable Beckers_et_al_2023_Tabulated_Database_Uloop_sCO2_heat

--- tests/test_geophires_x.py
+++ tests/test_geophires_x.py
@@ -216,7 +216,7 @@ class GeophiresXTestCase(unittest.TestCase):
                 # FIXME temporarily disabled unit tests (debugging GitHub Actions WIP)
                 and not example_file_path.startswith(
                     (
-                        'Beckers_et_al_2023_Tabulated_Database_Uloop_sCO2_heat',
+                        # 'Beckers_et_al_2023_Tabulated_Database_Uloop_sCO2_heat',
                         'Beckers_et_al_2023_Tabulated_Database_Coaxial_water_heat',
                         'Beckers_et_al_2023_Tabulated_Database_Coaxial_sCO2_heat',
                         'Beckers_et_al_2023_Tabulated_Database_Uloop_water_heat',

3d383933a7c9607bb7c0622927e6785c32855d4c
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-01-22 09:16:01 -0800

    Test example regeneration script

---
+++ tests/regenerate-example-result.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+
+# Use this script to regenerate example results in cases where changes in GEOPHIRES
+# calculations alter the example test output. Example:
+# ./tests/regenerate-example-result.sh SUTRAExample1
+
+
+cd "$(dirname "$0")"
+python -mgeophires_x examples/$1.txt examples/$1.out
+rm examples/$1.json

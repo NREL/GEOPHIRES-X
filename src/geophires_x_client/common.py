aed5196530434fa5b7f71441293f870d6e75364d
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-03-07 11:05:55 -0800

    Use __name__ instead of root for client result & common logger https://github.com/NREL/GEOPHIRES-X/issues/111#issuecomment-1964462981

--- src/geophires_x_client/common.py
+++ src/geophires_x_client/common.py
@@ -4,12 +4,15 @@ import sys
 _geophires_x_client_logger = None
 
 
-def _get_logger(logger_name='root'):
+def _get_logger(logger_name=None):
     global _geophires_x_client_logger
     if _geophires_x_client_logger is None:
         sh = logging.StreamHandler(sys.stdout)
         sh.setLevel(logging.INFO)
         sh.setFormatter(logging.Formatter(fmt='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+
+        if logger_name is None:
+            logger_name = __name__
 
         _geophires_x_client_logger = logging.getLogger(logger_name)
         _geophires_x_client_logger.addHandler(sh)

dc7ee918d0ab437b7aebbed85bdf91a1218803e8
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-03-11 12:28:12 -0700

    Bump version: 3.4.20 → 3.4.21

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ project = 'geophires-x'
 year = '2023'
 author = 'NREL'
 copyright = f'{year}, {author}'
-version = release = '3.4.20'
+version = release = '3.4.21'
 
 pygments_style = 'trac'
 templates_path = ['./templates']

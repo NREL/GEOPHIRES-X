e147275f7cb1928ba8519e0b855b41f08d9222f8
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-05-06 09:33:39 -0700

    Stub Fervo case study example

--- tests/test_geophires_x.py
+++ tests/test_geophires_x.py
@@ -152,7 +152,9 @@ class GeophiresXTestCase(BaseTestCase):
 
         example_files = list(
             filter(
-                lambda example_file_path: example_file_path.startswith(('example', 'Beckers_et_al', 'SUTRA', 'Wanju'))
+                lambda example_file_path: example_file_path.startswith(
+                    ('example', 'Beckers_et_al', 'SUTRA', 'Wanju', 'Fervo')
+                )
                 and '.out' not in example_file_path,
                 self._list_test_files_dir(test_files_dir='examples'),
             )

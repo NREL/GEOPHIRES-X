756e5174621ce39946acd1d123f9b1ef13184353
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-05-15 11:44:42 -0700

    Update FCR sensitivity test

--- tests/test_geophires_x.py
+++ tests/test_geophires_x.py
@@ -344,4 +344,4 @@ Print Output to Console, 1"""
 
         self.assertAlmostEqual(9.61, get_fcr_lcoe(0.05), places=1)
         self.assertAlmostEqual(3.33, get_fcr_lcoe(0.0001), places=1)
-        self.assertAlmostEqual(104.27, get_fcr_lcoe(0.8), places=1)
+        self.assertAlmostEqual(104.34, get_fcr_lcoe(0.8), places=0)

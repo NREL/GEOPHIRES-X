209cf9202f28e88b72927b92224c8eeada758d79
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-06-17 07:58:07 -0700

    Don't specify numpy 1.26 patch version - Actions fail https://github.com/NREL/GEOPHIRES-X/actions/runs/9549926828/job/26320675196\?pr\=231

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     keywords=['geothermal'],
     python_requires='>=3.7',
     install_requires=[
-        'numpy==1.26.4',
+        'numpy==1.26',
         'numpy-financial',
         'pint',
         'forex_python',

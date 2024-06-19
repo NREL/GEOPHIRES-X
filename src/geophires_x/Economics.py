9f8cca0d60ed6f77e12c8914ccf44a2a9a11a916
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-06-19 10:37:02 -0700

    Parametertize jobs created per MW per feedback https://github.com/NREL/GEOPHIRES-X/pull/240\#pullrequestreview-2128755164

--- src/geophires_x/Economics.py
+++ src/geophires_x/Economics.py
@@ -1509,6 +1509,15 @@ class Economics:
             ToolTipText="Production tax credit inflation adjusted"
         )
 
+        self.jobs_created_per_MW_electricity = self.ParameterDict[
+            self.jobs_created_per_MW_electricity.Name] = floatParameter(
+            "Estimated Jobs Created per MW of Electricity Produced",
+            DefaultValue=2.13,
+            UnitType=Units.NONE,
+            Required=False,
+            ToolTipText="Estimated jobs created per MW of electricity produced, per https://geothermal.org/resources/geothermal-basics"
+        )
+
         # local variable initialization
         self.CAPEX_cost_electricity_plant = 0.0
         self.CAPEX_cost_heat_plant = 0.0
@@ -2896,7 +2905,8 @@ class Economics:
 
         # https://github.com/NREL/GEOPHIRES-X/issues/232
         self.jobs_created.value = round(
-            np.average(model.surfaceplant.ElectricityProduced.quantity().to('MW').magnitude * 2.13))
+            np.average(model.surfaceplant.ElectricityProduced.quantity().to(
+                'MW').magnitude * self.jobs_created_per_MW_electricity.value))
 
         model.logger.info(f'complete {__class__!s}: {sys._getframe().f_code.co_name}')
 

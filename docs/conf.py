81661cfc962de1ca05c83398173bbf0a3c12f05d
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2024-10-01 10:32:46 -0700

    Bump version: 3.5.5 → 3.5.6

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ project = 'geophires-x'
 year = '2024'
 author = 'NREL'
 copyright = f'{year}, {author}'
-version = release = '3.5.5'
+version = release = '3.5.6'
 
 pygments_style = 'trac'
 templates_path = ['./templates']

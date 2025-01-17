9bb13109b1e28c0cc545da015abb4e950b83ca8a
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-01-17 10:15:55 -0800

    Bump version: 3.6.8 → 3.6.9

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ project = 'geophires-x'
 year = '2024'
 author = 'NREL'
 copyright = f'{year}, {author}'
-version = release = '3.6.8'
+version = release = '3.6.9'
 
 pygments_style = 'trac'
 templates_path = ['./templates']
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name='geophires-x',
-    version='3.6.8',
+    version='3.6.9',
     license='MIT',
     description='GEOPHIRES is a free and open-source geothermal techno-economic simulator.',
     long_description='{}\n{}'.format(

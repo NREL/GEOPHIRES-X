8c1b88db65b490b6b49006d5d53d69ee126a767a
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-01-21 07:46:44 -0800

    Revert "Bump version: 3.7.0 → 3.7.1"
    This reverts commit 2dfb4a93b69dde864dbebd5e44e760d77dc40e8a.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ project = 'geophires-x'
 year = '2024'
 author = 'NREL'
 copyright = f'{year}, {author}'
-version = release = '3.7.1'
+version = release = '3.7.0'
 
 pygments_style = 'trac'
 templates_path = ['./templates']

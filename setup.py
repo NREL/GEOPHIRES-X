48380381c0c00f9329ce76a6d8e3540741b789da
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-04-03 10:29:47 -0700

    Bump version: 3.8.10 → 3.8.11

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name='geophires-x',
-    version='3.8.10',
+    version='3.8.11',
     license='MIT',
     description='GEOPHIRES is a free and open-source geothermal techno-economic simulator.',
     long_description='{}\n{}'.format(

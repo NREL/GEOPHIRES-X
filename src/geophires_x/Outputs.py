3d295b623a33ffde83fcea8cdf119ee98c65637b
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-04-08 10:21:27 -0700

    SAM single-owner PPA economic model proof-of-concept

--- src/geophires_x/Outputs.py
+++ src/geophires_x/Outputs.py
@@ -246,7 +246,7 @@ class Outputs:
                     f.write(f'      Economic Model = {model.economics.econmodel.value.value}\n')
                     f.write(f'      {model.economics.interest_rate.Name}:                                    {model.economics.interest_rate.value:10.2f} {model.economics.interest_rate.CurrentUnits.value}\n')
 
-                elif model.economics.econmodel.value == EconomicModel.BICYCLE:
+                elif model.economics.econmodel.value in (EconomicModel.BICYCLE, EconomicModel.SINGLE_OWNER_PPA):
                     f.write(f'      Economic Model = {model.economics.econmodel.value.value}\n')
                 f.write(f'      Accrued financing during construction:            {model.economics.inflrateconstruction.value*100:10.2f} {model.economics.inflrateconstruction.CurrentUnits.value}\n')
                 f.write(f'      Project lifetime:                              {model.surfaceplant.plant_lifetime.value:10.0f} {model.surfaceplant.plant_lifetime.CurrentUnits.value}\n')

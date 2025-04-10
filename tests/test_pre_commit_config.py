061d13ebfdd09f7cb5c79a1f77979abbf0664c63
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-04-10 08:56:51 -0700

    attempt to include EconomicsSam.py with pre-commit but doesn't work, giving up for now

---
+++ tests/test_pre_commit_config.py
@@ -0,0 +1,18 @@
+import re
+import unittest
+
+
+class PreCommitConfigTestCase(unittest.TestCase):
+    """
+    Utility for making updates to .pre-commit-config.yaml
+    """
+
+    def test_pre_commit_exclude_pattern(self):
+        pattern = r'^(\.tox|ci/templates|\.bumpversion\.cfg|src/geophires_x(?!/(EconomicsSam|GEOPHIRESv3)\.py))(/|$)'
+        self.assertFalse(re.match(pattern, 'src/geophires_x/GEOPHIRESv3.py'))
+        self.assertFalse(re.match(pattern, 'src/geophires_x/EconomicsSam.py'))
+        self.assertTrue(re.match(pattern, 'src/geophires_x/Economics.py'))
+
+
+if __name__ == '__main__':
+    unittest.main()

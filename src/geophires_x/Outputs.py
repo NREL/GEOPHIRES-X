NREL/GEOPHIRES-X

commit 46522ace00c18aa7e5e4ed6442c23bad91c1e762

output interest rate (tangentially related to WIP to pass it to SAM)

src/geophires_x/Outputs.py
@@ -250,6 +250,13 @@ class Outputs:
 
                 elif model.economics.econmodel.value in (EconomicModel.BICYCLE, EconomicModel.SAM_SINGLE_OWNER_PPA):
                     f.write(f'      Economic Model = {model.economics.econmodel.value.value}\n')
+
+                if model.economics.econmodel.value in EconomicModel.SAM_SINGLE_OWNER_PPA:
+                    # TODO disambiguate interest rate for all economic models - see
+                    #  535c02d4adbeeeca553b61e9b996fccf00016529
+                    irfl = Outputs._field_label(econ.interest_rate.Name, 49)
+                    f.write(f'      {irfl}{econ.interest_rate.value:10.2f} {econ.interest_rate.CurrentUnits.value}\n')
+
                 f.write(f'      Accrued financing during construction:            {model.economics.inflrateconstruction.value*100:10.2f} {model.economics.inflrateconstruction.CurrentUnits.value}\n')
                 f.write(f'      Project lifetime:                              {model.surfaceplant.plant_lifetime.value:10.0f} {model.surfaceplant.plant_lifetime.CurrentUnits.value}\n')
                 f.write(f'      Capacity factor:                                 {model.surfaceplant.utilization_factor.value * 100:10.1f} %\n')

src/geophires_x/EconomicsSam.py
@@ -138,7 +138,8 @@ def _get_single_owner_parameters(model: Model) -> dict[str, Any]:
     # Debt/equity ratio ('Fraction of Investment in Bonds' parameter)
     ret['debt_percent'] = econ.FIB.quantity().to(convertible_unit('%')).magnitude
 
-    # TODO interest rate
+    # TODO/WIP interest rate
+    # ret['real_discount_rate']
 
     # 'Property Tax Rate'
     ret['property_tax_rate'] = econ.PTR.quantity().to(convertible_unit('%')).magnitude

tests/geophires_x_tests/test_economics_sam.py
@@ -25,7 +25,7 @@ from geophires_x_client import GeophiresXResult
 class EconomicsSamTestCase(BaseTestCase):
 
     def _egs_test_file_path(self) -> str:
-        return self._get_test_file_path('generic-egs-case-2.txt')
+        return self._get_test_file_path('generic-egs-case-2_sam-single-owner-ppa.txt')
 
     def _get_result(self, _params) -> GeophiresXResult:
         return GeophiresXClient().get_geophires_result(
@@ -45,6 +45,10 @@ class EconomicsSamTestCase(BaseTestCase):
         base_result = self._get_result({})
         base_lcoe = _lcoe(base_result)
         self.assertGreater(base_lcoe, 7)
+
+        ir = base_result.result['ECONOMIC PARAMETERS']['Interest Rate']
+        self.assertEqual(ir['value'], 7.0)
+        self.assertEqual(ir['unit'], '%')
 
         npvs = [_npv(self._get_result({'Starting Electricity Sale Price': x / 100.0})) for x in range(1, 20, 4)]
         for i in range(len(npvs) - 1):

commit 30a9739d14f9a69984a6d9c18b17cbb170529948

fix unit test failure caused by error introduced in https://github.com/softwareengineerprogrammer/GEOPHIRES/commit/46522ace00c18aa7e5e4ed6442c23bad91c1e762#diff-af6ce00466ae6049eed836cdf739c02412c76afa51c69c5e62dc04c04f86053bR254-R256

src/geophires_x/Outputs.py
@@ -251,7 +251,7 @@ class Outputs:
                 elif model.economics.econmodel.value in (EconomicModel.BICYCLE, EconomicModel.SAM_SINGLE_OWNER_PPA):
                     f.write(f'      Economic Model = {model.economics.econmodel.value.value}\n')
 
-                if model.economics.econmodel.value in EconomicModel.SAM_SINGLE_OWNER_PPA:
+                if model.economics.econmodel.value == EconomicModel.SAM_SINGLE_OWNER_PPA:
                     # TODO disambiguate interest rate for all economic models - see
                     #  535c02d4adbeeeca553b61e9b996fccf00016529
                     irfl = Outputs._field_label(econ.interest_rate.Name, 49)
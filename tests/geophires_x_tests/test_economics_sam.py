NREL/GEOPHIRES-X

commit d6966a642ac9d71a22743d25755e6321bb512251

pass inflation to SAM

src/geophires_x/EconomicsSam.py
@@ -49,6 +49,9 @@ def calculate_sam_economics(model: Model) -> dict[str, dict[str, Any]]:
                 if k != 'number_inputs':
                     module.value(k, v)
 
+    for k, v in _get_utility_rate_parameters(model).items():
+        single_owner.value(k, v)
+
     for k, v in _get_single_owner_parameters(model).items():
         single_owner.value(k, v)
 
@@ -84,6 +87,16 @@ def calculate_sam_economics(model: Model) -> dict[str, dict[str, Any]]:
     return ret
 
 
+def _get_utility_rate_parameters(model: Model) -> dict[str, Any]:
+    econ = model.economics
+
+    ret: dict[str, Any] = {}
+
+    ret['inflation_rate'] = econ.RINFL.quantity().to(convertible_unit('%')).magnitude
+
+    return ret
+
+
 def _get_single_owner_parameters(model: Model) -> dict[str, Any]:
     econ = model.economics
 
@@ -95,8 +108,10 @@ def _get_single_owner_parameters(model: Model) -> dict[str, Any]:
 
     opex_musd = econ.Coam.value
     ret['om_fixed'] = [opex_musd * 1e6]
+    # GEOPHIRES assumes O&M fixed costs are not affected by inflation
+    ret['om_fixed_escal'] = -1.0 * econ.RINFL.quantity().to(convertible_unit('%')).magnitude
 
-    # FIXME provide entire generation profile
+    # FIXME TODO provide entire generation profile
     average_net_generation_MW = _get_average_net_generation_MW(model)
     ret['system_capacity'] = average_net_generation_MW * 1e3
 
@@ -122,8 +137,6 @@ def _get_single_owner_parameters(model: Model) -> dict[str, Any]:
 
     # Debt/equity ratio ('Fraction of Investment in Bonds' parameter)
     ret['debt_percent'] = econ.FIB.quantity().to(convertible_unit('%')).magnitude
-
-    # TODO inflation (econ.RINFL.value)
 
     # TODO interest rate
 

commit cd8fc676b4e880139cc528056141a976b4d33e40

fix unit test failure introduced in d6966a642ac9d71a22743d25755e6321bb512251 by keeping electricity rate percent constant

src/geophires_x/EconomicsSam.py
@@ -27,6 +27,7 @@ from geophires_x.EconomicsSamCashFlow import _calculate_sam_economics_cash_flow
 from geophires_x.Units import convertible_unit
 
 _SAM_CASH_FLOW_PROFILE_KEY = 'Cash Flow'
+_GEOPHIRES_TO_SAM_PRICING_MODEL_RATE_CONVERSION_CONSTANT = 0.745
 
 
 @lru_cache(maxsize=12)
@@ -132,7 +133,12 @@ def _get_single_owner_parameters(model: Model) -> dict[str, Any]:
 
     ret['ppa_price_input'] = [econ.ElecStartPrice.value]
     # Approximation of GEOPHIRES rate model into SAM's percent inflation model (TODO - could probably be improved)
-    ppa_escalation_rate_percent = round(econ.ElecEscalationRate.value / econ.ElecStartPrice.value * 0.745 * 100.0)
+    ppa_escalation_rate_percent = round(
+        econ.ElecEscalationRate.value
+        / econ.ElecStartPrice.value
+        * _GEOPHIRES_TO_SAM_PRICING_MODEL_RATE_CONVERSION_CONSTANT
+        * 100.0
+    )
     ret['ppa_escalation'] = ppa_escalation_rate_percent
 
     # Debt/equity ratio ('Fraction of Investment in Bonds' parameter)

tests/geophires_x_tests/test_economics_sam.py
@@ -13,7 +13,12 @@ from base_test_case import BaseTestCase
 from geophires_x.Model import Model
 
 # noinspection PyProtectedMember
-from geophires_x.EconomicsSam import calculate_sam_economics, _sig_figs, _SAM_CASH_FLOW_PROFILE_KEY
+from geophires_x.EconomicsSam import (
+    calculate_sam_economics,
+    _sig_figs,
+    _SAM_CASH_FLOW_PROFILE_KEY,
+    _GEOPHIRES_TO_SAM_PRICING_MODEL_RATE_CONVERSION_CONSTANT,
+)
 
 # noinspection PyProtectedMember
 from geophires_x.EconomicsSamCashFlow import _clean_profile
@@ -50,7 +55,19 @@ class EconomicsSamTestCase(BaseTestCase):
         self.assertEqual(ir['value'], 7.0)
         self.assertEqual(ir['unit'], '%')
 
-        npvs = [_npv(self._get_result({'Starting Electricity Sale Price': x / 100.0})) for x in range(1, 20, 4)]
+        inflation = 0.02
+        rate_params = [
+            {
+                'Starting Electricity Sale Price': x / 100.0,
+                # Escalation rate must remain constant percent
+                'Electricity Escalation Rate Per Year': x
+                * inflation
+                / 100.0
+                / _GEOPHIRES_TO_SAM_PRICING_MODEL_RATE_CONVERSION_CONSTANT,
+            }
+            for x in range(1, 20, 4)
+        ]
+        npvs = [_npv(self._get_result(rp)) for rp in rate_params]
         for i in range(len(npvs) - 1):
             self.assertLess(npvs[i], npvs[i + 1])
 
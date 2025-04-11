043297fb755bad99b3b36cf6ba1bc6bfa538ea4c
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-04-11 07:34:51 -0700

    move cash flow function to EconomicsSamCashFlow.py

--- tests/test_pre_commit_config.py
+++ tests/test_pre_commit_config.py
@@ -8,9 +8,10 @@ class PreCommitConfigTestCase(unittest.TestCase):
     """
 
     def test_pre_commit_exclude_pattern(self):
-        pattern = r'^(\.tox|ci/templates|\.bumpversion\.cfg|src/geophires_x(?!/(EconomicsSam|GEOPHIRESv3)\.py))(/|$)'
+        pattern = r'^(\.tox|ci/templates|\.bumpversion\.cfg|src/geophires_x(?!/(GEOPHIRESv3|EconomicsSam|EconomicsSamCashFlow)\.py))(/|$)'
         self.assertFalse(re.match(pattern, 'src/geophires_x/GEOPHIRESv3.py'))
         self.assertFalse(re.match(pattern, 'src/geophires_x/EconomicsSam.py'))
+        self.assertFalse(re.match(pattern, 'src/geophires_x/EconomicsSamCashFlow.py'))
         self.assertTrue(re.match(pattern, 'src/geophires_x/Economics.py'))
 
 

cff8dcb97754c60e719abf973f4a1a5620f4a658
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-04-14 14:06:12 -0700

    remove code that was moved to geophires_sam_economics

--- src/geophires_x/EconomicsSamCashFlow.py
+++ src/geophires_x/EconomicsSamCashFlow.py
@@ -180,6 +180,8 @@ _SINGLE_OWNER_OUTPUT_PROPERTIES_ADDITIONAL = {
     # TODO: unclear what this is derived from
     # 'Other financing cost ($)'
 }
+
+
 # fmt:on
 
 
@@ -308,32 +310,3 @@ def _get_single_owner_output(soo: Any, display_name: str) -> Any:
         return prop(soo)
 
     return getattr(soo, prop)
-
-
-def _generate_mapping_from_module_doc(module_name: str = 'Cashloan'):
-    with open(_get_file_path(f'sam_economics/{module_name}.html.txt'), encoding='utf-8') as f:
-        # lines = f.readlines()
-
-        txt = f.read()
-        # lines = txt.split('PySAM.Cashloan.Cashloan.Outputs')[1].split('\n')
-        txt = txt.split(f'PySAM.{module_name}.{module_name}.Outputs')[1].split('Outputs_vals =')[2]
-        lines = txt.split('\n')[2:]
-
-        # _log.debug(f'Found {len(lines)} lines {module_name} in module doc')
-        for i in range(0, len(lines), 8):
-            prop = lines[i].strip()[:-1]
-            if prop == '':
-                continue
-            display_name = lines[i + 2].strip().replace('[', '(').replace(']', ')')
-            # _log.debug(f'Property: {prop}')
-            if display_name not in _SINGLE_OWNER_OUTPUT_PROPERTIES:
-                print(f"\t'{display_name}': '{prop}',  # {module_name}")
-
-
-if __name__ == '__main__':
-    # sop = _SINGLE_OWNER_OUTPUT_PROPERTIES.copy()
-    # # del sop['Total installed cost ($)']
-    # print(json_dumpse(sop))
-
-    _generate_mapping_from_module_doc('Cashloan')
-    _generate_mapping_from_module_doc('Singleowner')

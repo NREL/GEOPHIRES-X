8e66c57c9801a61ee734da6e70d262b76f67095d
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-04-17 10:51:16 -0700

    finish integrating capacity/utilization factor, including resolution of a few incidental nits discovered in the course of implementation

--- tests/test_base_test_case.py
+++ tests/test_base_test_case.py
@@ -25,6 +25,25 @@ class TestBaseTestCase(BaseTestCase):
         with self.assertRaises(AssertionError):
             self.assertListAlmostEqual([1, 2, 3], [1.1, 2.2, 3.3], percent=5)
 
+    def test_assertAlmostEqualWithinPercentage_bad_arguments(self):
+        with self.assertRaises(ValueError) as msg_type_error:
+            self.assertAlmostEqualWithinPercentage(100, 100, 10)
+
+            self.assertIn(str(msg_type_error), '(you may have meant to pass percent=10)')
+
+        def assertHasLogRecordWithMessage(logs_, message):
+            assert message in [record.message for record in logs_.records]
+
+        with self.assertLogs(level='INFO') as logs:
+            with self.assertRaises(AssertionError):
+                self.assertAlmostEqualWithinPercentage([1, 2, 3], [1.1, 2.2, 3.3], percent=10.5)
+
+            assertHasLogRecordWithMessage(
+                logs,
+                'Got 2 lists, you probably meant to call:\n\t'
+                'self.assertListAlmostEqual([1, 2, 3], [1.1, 2.2, 3.3], msg=None, percent=10.5)',
+            )
+
 
 if __name__ == '__main__':
     unittest.main()

c04652603a47b17fac087b3f7d1cedbf8d891243
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-05-01 10:38:32 -0700

    Conditionally set matplotlib backend to Agg only if running in GitHub Actions and OS is windows, since GHA macos apparently doesn't like Agg backend (https://github.com/softwareengineerprogrammer/GEOPHIRES/actions/runs/14779862524/job/41496253000\#step:5:721)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,10 @@
-import matplotlib
+import os
 
-# Backend should be 'Agg' in GitHub Actions to prevent intermittent Windows failures
-# per https://github.com/NREL/GEOPHIRES-X/issues/365
-print(f'[DEBUG] matplotlib backend: {matplotlib.get_backend()}')
+import matplotlib as mpl
+
+if os.name == 'nt' and 'TOXPYTHON' in os.environ:
+    # Backend should be 'Agg' in GitHub Actions to prevent intermittent Windows failures
+    # per https://github.com/NREL/GEOPHIRES-X/issues/365
+    mpl.use('Agg')
+
+print(f'[DEBUG] matplotlib backend: {mpl.get_backend()}')

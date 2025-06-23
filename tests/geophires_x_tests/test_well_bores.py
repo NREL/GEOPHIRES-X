ef432658c060972fda0178452ad3c95a1fcf6f36
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-06-23 09:50:00 -0700

    test_number_of_doublets_non_integer to ensure MC compatibility

--- tests/geophires_x_tests/test_well_bores.py
+++ tests/geophires_x_tests/test_well_bores.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-
 from base_test_case import BaseTestCase
 
 # ruff: noqa: I001  # Successful module initialization is dependent on this specific import order.
@@ -16,57 +15,24 @@ from geophires_x_client import GeophiresXResult
 class WellBoresTestCase(BaseTestCase):
 
     def test_number_of_doublets(self):
-        def _get_result(_params) -> GeophiresXResult:
-            params = GeophiresInputParameters(
-                {
-                    'Reservoir Depth': 5,
-                    'Gradient 1': 74,
-                    'Power Plant Type': 2,
-                    'Maximum Temperature': 600,
-                }
-                | _params
-            )
-            return GeophiresXClient().get_geophires_result(params)
-
-        def _prod_inj_lcoe(_r: GeophiresXResult) -> tuple[int, int]:
-            return (
-                _r.result['ENGINEERING PARAMETERS']['Number of Production Wells']['value'],
-                _r.result['ENGINEERING PARAMETERS']['Number of Injection Wells']['value'],
-                _r.result['SUMMARY OF RESULTS']['Electricity breakeven price']['value'],
-                _r.result['SUMMARY OF RESULTS']['Electricity breakeven price']['value'],
-                _r.result['SURFACE EQUIPMENT SIMULATION RESULTS']['Average Net Electricity Generation']['value'],
-            )
-
-        r_prod_inj: GeophiresXResult = _get_result(
+        r_prod_inj: GeophiresXResult = self._get_result(
             {
                 'Number of Production Wells': 10,
                 'Number of Injection Wells': 10,
             }
         )
 
-        r_doublets: GeophiresXResult = _get_result(
+        r_doublets: GeophiresXResult = self._get_result(
             {
                 'Number of Doublets': 10,
             }
         )
 
-        self.assertEqual(_prod_inj_lcoe(r_doublets), _prod_inj_lcoe(r_prod_inj))
+        self.assertEqual(self._prod_inj_lcoe_production(r_doublets), self._prod_inj_lcoe_production(r_prod_inj))
 
     def test_number_of_doublets_validation(self):
-        def _get_result(_params) -> GeophiresXResult:
-            params = GeophiresInputParameters(
-                {
-                    'Reservoir Depth': 5,
-                    'Gradient 1': 74,
-                    'Power Plant Type': 2,
-                    'Maximum Temperature': 600,
-                }
-                | _params
-            )
-            return GeophiresXClient().get_geophires_result(params)
-
         with self.assertRaises(RuntimeError):
-            _get_result(
+            self._get_result(
                 {
                     'Number of Production Wells': 10,
                     'Number of Injection Wells': 10,
@@ -75,7 +41,7 @@ class WellBoresTestCase(BaseTestCase):
             )
 
         with self.assertRaises(RuntimeError):
-            _get_result(
+            self._get_result(
                 {
                     'Number of Production Wells': 10,
                     'Number of Doublets': 10,
@@ -83,9 +49,59 @@ class WellBoresTestCase(BaseTestCase):
             )
 
         with self.assertRaises(RuntimeError):
-            _get_result(
+            self._get_result(
                 {
                     'Number of Injection Wells': 10,
                     'Number of Doublets': 10,
                 }
             )
+
+    def test_number_of_doublets_non_integer(self):
+        """
+        Non-integer values are relevant for MC simulations, since distributions produce floats, and we want
+        Number of Doublets to be compatible with MC.
+        """
+
+        prod_inj_lcoe = self._prod_inj_lcoe_production(
+            self._get_result(
+                {
+                    'Number of Doublets': 40.7381,
+                }
+            )
+        )
+
+        self.assertEqual(prod_inj_lcoe[0], 40)
+        self.assertEqual(prod_inj_lcoe[1], 40)
+
+        prod_inj_lcoe_2 = self._prod_inj_lcoe_production(
+            self._get_result(
+                {
+                    'Number of Doublets': 199.2,
+                }
+            )
+        )
+
+        self.assertEqual(prod_inj_lcoe_2[0], 199)
+        self.assertEqual(prod_inj_lcoe_2[1], 199)
+
+    # noinspection PyMethodMayBeStatic
+    def _get_result(self, _params) -> GeophiresXResult:
+        params = GeophiresInputParameters(
+            {
+                'Reservoir Depth': 5,
+                'Gradient 1': 74,
+                'Power Plant Type': 2,
+                'Maximum Temperature': 600,
+            }
+            | _params
+        )
+        return GeophiresXClient().get_geophires_result(params)
+
+    # noinspection PyMethodMayBeStatic
+    def _prod_inj_lcoe_production(self, _r: GeophiresXResult) -> tuple[int, int, float, float]:
+        return (
+            _r.result['ENGINEERING PARAMETERS']['Number of Production Wells']['value'],
+            _r.result['ENGINEERING PARAMETERS']['Number of Injection Wells']['value'],
+            _r.result['SUMMARY OF RESULTS']['Electricity breakeven price']['value'],
+            _r.result['SURFACE EQUIPMENT SIMULATION RESULTS']['Average Net Electricity Generation']['value'],
+        )

263bba25a9f705936837c3e2c5d8c683826e319f
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2025-07-23 11:09:19 -0700

    Root cause of intermittent failures may have been introduced in 9ad5d2a9265080c6ff07e48459c8c0181afdae09 due to schema generator adding log handlers. Address by reverting previous workaround strategy (bf0000c, 12fab1a), updating client _get_logger method to store loggers by name, and using it instead of an ad hoc implementation in the schema generator.

--- tests/test_base_test_case.py
+++ tests/test_base_test_case.py
@@ -1,6 +1,3 @@
-import os
-import platform
-import sys
 import unittest
 
 from tests.base_test_case import BaseTestCase
@@ -38,26 +35,11 @@ class TestBaseTestCase(BaseTestCase):
             with self.assertRaises(AssertionError):
                 self.assertAlmostEqualWithinPercentage([1, 2, 3], [1.1, 2.2, 3.3], percent=10.5)
 
-            try:
-                self.assertHasLogRecordWithMessage(
-                    logs,
-                    'Got 2 lists, you probably meant to call:\n\t'
-                    'self.assertListAlmostEqual([1, 2, 3], [1.1, 2.2, 3.3], msg=None, percent=10.5)',
-                )
-            except AssertionError as ae:
-                if (
-                    'CI' in os.environ
-                    and (platform.system() in ['Darwin', 'Linux'])
-                    and (sys.version_info.major, sys.version_info.minor) == (3, 11)
-                ):
-                    # Intermittent failures observed in GitHub Actions py311 macos and ubuntu beginning on 2025-07-23.
-                    # Examples:
-                    # - https://github.com/softwareengineerprogrammer/GEOPHIRES/actions/runs/16476574734/job/46579711905
-                    # - https://github.com/softwareengineerprogrammer/GEOPHIRES/actions/runs/16477002832/job/46581220253
-                    # TODO to investigate and resolve
-                    self.skipTest(f'Skipping test due to platform-specific intermittent failure: {ae!s}')
-                else:
-                    raise ae
+            self.assertHasLogRecordWithMessage(
+                logs,
+                'Got 2 lists, you probably meant to call:\n\t'
+                'self.assertListAlmostEqual([1, 2, 3], [1.1, 2.2, 3.3], msg=None, percent=10.5)',
+            )
 
 
 if __name__ == '__main__':
